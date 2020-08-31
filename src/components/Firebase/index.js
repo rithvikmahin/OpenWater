60883be55e4d29a8b8f698b72e0d29cb931f913a
Rithvik <rithvik7@illinois.edu>
2020-08-31 00:43:17 -0400

    Fixed a typo 'FireBase' -> 'Firebase'.

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -2,5 +2,4 @@ import FirebaseContext from './context';
 import Firebase from './firebase';
  
 export default Firebase;
- 
 export { FirebaseContext };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import FireBase, { FireBaseContext } from './components/Firebase';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>

7f107129c89cd52f1c131ab52f33a7a467b04d22
Rithvik <rithvik7@illinois.edu>
2020-08-31 21:30:11 -0400

    Added the home logo and buttons, enabled chat in Firebase.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,21 +8,21 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="home">
-        <Header></Header>
         <section>
           <div className="jumbotron jumbotron-fluid py-5">
             <div className="container text-center py-5">
                 <div className='image-container'>
                   <img src={ require('../assets/images/Logo.png') } className='center' />
                 </div>
-              <div className="mt-4">
-                <Link className="sign-up" to="/signup"><span className="sign-up-font">SIGN UP</span></Link>
-                <Link className="sign-in" to="/login"><span className="sign-in-font">SIGN IN</span></Link>
+                
+                  <div className='button-container'>
+                    <Link className="sign-up" to="/signup"><span className="sign-up-font">SIGN UP</span></Link>
+                    <Link className="sign-in" to="/login"><span className="sign-in-font">SIGN IN</span></Link>
+                  </div>
+
               </div>
-            </div>
           </div>
         </section>
-        <Footer></Footer>
       </div>
     )
   }

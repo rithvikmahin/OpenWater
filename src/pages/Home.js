afa3f6b629a2829f8e9075a3ff9f7a859c0cd503
Rithvik <rithvik7@illinois.edu>
2020-08-31 21:11:46 -0400

    Began redesigning the home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
+import '../assets/css/home.css'
 
 export default class HomePage extends Component {
   render() {
@@ -11,10 +12,12 @@ export default class HomePage extends Component {
         <section>
           <div className="jumbotron jumbotron-fluid py-5">
             <div className="container text-center py-5">
-              <h1 className="display-4">Clinque</h1>
+                <div className='image-container'>
+                  <img src={ require('../assets/images/Logo.png') } className='center' />
+                </div>
               <div className="mt-4">
-                <Link className="btn btn-primary px-5 mr-3" to="/signup">Create New Account</Link>
-                <Link className="btn px-5" to="/login">Login to Your Account</Link>
+                <Link className="sign-up" to="/signup"><span className="sign-up-font">SIGN UP</span></Link>
+                <Link className="sign-in" to="/login"><span className="sign-in-font">SIGN IN</span></Link>
               </div>
             </div>
           </div>
